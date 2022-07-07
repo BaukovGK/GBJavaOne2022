6c6b46cbb059fc8cec3df793f93b24024eee88d3
BaukovGK <glebabaukov@yandex.ru>
2022-07-07 16:32:34 +0300

    HomeWork1

---
+++ src/main/java/Lesoon21/Trampoline.java
@@ -0,0 +1,16 @@
+package Lesoon21;
+
+public class Trampoline implements Action{
+    private int distance;
+    public Trampoline (int distance){
+        this.distance = distance;
+    }
+
+    public int running ()
+    {
+        return distance;
+    }
+    public int jumping() {
+        return 0;
+    }
+}
